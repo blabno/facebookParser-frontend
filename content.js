570e8055df72880c9b071fddfeb9b4c0780221f9
Velkan <achwatm@gmail.com>
2019-08-05 14:42:06 +0200

    list of post images

--- content.js
+++ content.js
@@ -8,7 +8,8 @@ var Post = class Post {
         this.image = image;
     }
 };
-function deleteSeeMore(text, del){
+
+function deleteSeeMore(text, del) {
 
 }
 
@@ -20,25 +21,31 @@ chrome.runtime.onMessage.addListener(function (msg, sender, sendResponse) {
             var list = [];
             var len = document.querySelectorAll(".userContent").length;
             var more = document.querySelectorAll('.see_more_link');
-            for (let i = 0; i < more.length; i++) {
-                console.log(1);
-                document.querySelectorAll('.see_more_link')[i].click();
-            }
-            for (let i = 0; i < len; i++) {
+            for (let i = 0; i < len - 1; i++) {
 
                 var id = document.querySelector("#pagelet_group_mall").querySelector("div").querySelectorAll("._4-u2, .mbm, ._4mrt, ._5jmm, ._5pat, ._5v3q, ._7cqq, ._4-u8")[i].getAttribute('id');
-                var author = document.querySelectorAll(".fwn .fcg")[i].textContent;
-                var content = document.querySelectorAll(".userContent")[i].textContent;
-                if( document.querySelectorAll(".userContent")[i].querySelector(".text_exposed_show") !== null){
-
-                    content += document.querySelectorAll(".userContent")[i].querySelector(".text_exposed_show").textContent;
+                var author = document.querySelectorAll('.userContentWrapper')[i].querySelector(".fwn .fcg").textContent;
+                var content = document.querySelectorAll('.userContentWrapper')[i].querySelector(".userContent").textContent;
+                if (document.querySelectorAll('.userContentWrapper')[i].querySelector(".userContent").querySelector(".text_exposed_show") !== null) {
+                    content = content.replace('...', '');
+                    content = content.replace('Zobacz więcej', '');
+                    console.log(i);
+                    //content += document.querySelectorAll(".userContent")[i].querySelector(".text_exposed_show").textContent;
                 }
-                var date = document.querySelectorAll('abbr._5ptz')[i].getAttribute('title');
-                var image;
-                if (document.querySelectorAll('.userContentWrapper')[i].querySelector('._4-eo') !== null) {
-                    image = document.querySelectorAll('.userContentWrapper')[i].querySelector('._4-eo').getAttribute('href');
-                } else {
-                   image = '';
+                var date = document.querySelectorAll('.userContentWrapper')[i].querySelector('abbr._5ptz').getAttribute('title');
+            var image =[];
+console.log(document.querySelectorAll('.userContentWrapper')[i].querySelectorAll('._5dec').length);
+                if (document.querySelectorAll('.userContentWrapper')[i].querySelectorAll('._4-eo, ._2t9n').length === 1) {
+                    image = [document.querySelectorAll('.userContentWrapper')[i].querySelector('._4-eo, ._2t9n').querySelector('img').getAttribute('src')];
+                } else if (document.querySelectorAll('.userContentWrapper')[i].querySelectorAll('._5dec').length > 1) {
+                    let lenOfImages = document.querySelectorAll('.userContentWrapper')[i].querySelectorAll('._5dec, ._xcx, ._487t').length;
+                    console.log('len:'+lenOfImages);
+                    for (let j = 0; j < lenOfImages; j++) {
+                        console.log('j:'+ j);
+                        image.push(document.querySelectorAll('.userContentWrapper')[i].querySelectorAll('._5dec, ._xcx, ._487t')[j].querySelector('img').getAttribute('src'));
+                        console.log(image);
+                    }
+                    console.log('lista:' + image);
                 }
 
                 let post = new Post(id, author, date, content, image);
