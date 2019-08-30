e58f4875aea5dd48c20cb2d92c5734070ee71b68
Velkan <achwatm@gmail.com>
2019-08-30 12:40:44 +0200

    adding-group-id-to-path

--- background.js
+++ background.js
@@ -1,21 +1,29 @@
 const urlRegex = new RegExp(/^https?:\/\/(?:[^./?#]+\.)?facebook\.com/);
-
+let pathVariable = '' ;
 function sendPosts(list) {
   for (let i = 0; i < list.length; i++) {
     var xhr = new XMLHttpRequest();
-    xhr.open("POST", "http://192.168.0.101:3000/", true);
+    xhr.open("POST", "http://192.168.0.124:3000/"+pathVariable, true);
     xhr.send(JSON.stringify(list[i]));
   }
 }
 
 function httpGet() {
   var xmlHttp = new XMLHttpRequest();
-  xmlHttp.open("GET", 'http://192.168.0.101:3000/last', false);
+  xmlHttp.open("GET", 'http://192.168.0.124:3000/last/'+pathVariable, false);
   xmlHttp.send(null);
   return xmlHttp.responseText;
 }
 
 chrome.browserAction.onClicked.addListener(function (tab) {
+    chrome.tabs.query({
+        active: true,
+        currentWindow: true
+    }, function (tabs) {
+        let url = tabs[0].url.substring(7);
+        let urlParts = url.split('/');
+        pathVariable = urlParts[3];
+    });
   if (urlRegex.test(tab.url)) {
     chrome.tabs.sendMessage(tab.id, {text: httpGet()}, sendPosts);
   } else {
